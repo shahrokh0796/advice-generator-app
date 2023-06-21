d3e354e7cb835dbc57e168baa30d854f2bce3f50
Said <shahrokh07.96@gmail.com>
2023-06-21 20:11:58 +0530

    Advice generator app completed

--- src/index.js
+++ src/index.js
@@ -1,2 +1,28 @@
 import "./index.html";
 import "./styles/styles.scss";
+
+async function getAdviceApI() {
+    try {
+        const randomAdvice = await fetch("https://api.adviceslip.com/advice");
+        if (!randomAdvice.ok) {
+            throw new Error(`Responce Error: ${randomAdvice.status}`);
+        }
+
+        const randomAdviceJSON = await randomAdvice.json();
+        const adviceId = document.querySelector("#advice-id");
+        const advice = document.querySelector("#advice");
+        for (let prop in randomAdviceJSON) {
+            console.log(randomAdviceJSON[prop].id+" "+randomAdviceJSON[prop].advice);
+            adviceId.textContent = randomAdviceJSON[prop].id;
+            advice.textContent = `"${randomAdviceJSON[prop].advice}"`;
+        }
+
+        document.querySelector("#dice").addEventListener("click", () => {
+            location.reload(true);
+        });
+    } catch (error) {
+        console.log("This is the error: ", error);
+    }
+}
+
+getAdviceApI();
